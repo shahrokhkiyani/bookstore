f5a9ca642bb7393e576fdb27a45e5982fc11b7a1
Shahrokh Kiyani <shahshahrokh@protonmail.com>
2022-03-27 00:09:36 +0430

    Created - Custom User Model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from accounts.models import CustomUser
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ['username', 'email', 'age', 'is_staff']
+    fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("age",)}),)
+
+    add_fieldsets = UserAdmin.add_fieldsets + ((None, {"fields": ("age",)}),)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
---
+++ accounts/forms.py
@@ -0,0 +1,19 @@
+from dataclasses import field
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+from .models import CustomUser
+
+# TODO: User Creation Form
+# TODO: User Change Form
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = UserCreationForm.Meta.fields + ('age', )
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = CustomUser
+        fields = UserChangeForm.Meta.fields
+        
