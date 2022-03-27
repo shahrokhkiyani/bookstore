e222331d7f62f0285db1573cf110562dad719bc7
Shahrokh Kiyani <shahshahrokh@protonmail.com>
2022-03-27 12:35:18 +0430

    added email section on signup and reformatted some files

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,17 +3,23 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 from .models import CustomUser
 
-# TODO: User Creation Form
-# TODO: User Change Form
-
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields = UserCreationForm.Meta.fields + ("age",)
+        # fields = UserCreationForm.Meta.fields + ("age", "email")
+        fields = (
+            "username",
+            "age",
+            "email",
+        )
 
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
-        fields = UserChangeForm.Meta.fields
+        fields = (
+            "username",
+            "age",
+            "email",
+        )
