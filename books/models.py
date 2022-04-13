b5a876f14915f14aab02ad46fb6c6d81fc57d5d1
Shahrokh Kiyani <shahrokhkiyani@protonmail.com>
2022-04-13 12:13:33 +0430

    created book model and adedd something in that model

--- books/models.py
+++ books/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 
 class Book(models.Model):
+    
     # name, description, author, creator, date_time published, cover, publisher, year created
     
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    content = models.TextField()
+    #? desimal_places number of persian -> "ashar's" in nubmer
+    #? max_digits number of digits in number like -> 1234.56
+    #? For money we use DeciamlFields
+    price= models.DecimalField(desimal_places=2 ,max_digits=5)
     
+
