f965c89155941c44f20f69d1c8cee2b729ba5d34
Shahrokh Kiyani <shahrokhkiyani@protonmail.com>
2022-04-13 19:50:00 +0430

    added book to admin

--- books/models.py
+++ books/models.py
@@ -1,18 +1,19 @@
 from django.db import models
 
+
 class Book(models.Model):
-    
+
     # name, description, author, creator, date_time published, cover, publisher, year created
-    
+
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     content = models.TextField()
-    #? desimal_places number of persian -> "ashar's" in nubmer
+    # ? desimal_places number of persian -> "ashar's" in nubmer
 
-    #? max_digits number of digits in number like -> 1234.56
+    # ? max_digits number of digits in number like -> 1234.56
 
-    #? For money we use DeciamlFields
-    price= models.DecimalField(max_digits=5, decimal_places=2)
-    
+    # ? For money we use DeciamlFields
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
     def __str__(self):
         return self.title
