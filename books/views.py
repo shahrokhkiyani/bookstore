b52e382772796bf4f41071a4bd6a7a5bdd6fd5f3
Shahrokh Kiyani <shahrokhkiyani@protonmail.com>
2022-04-19 19:20:39 +0430

    created. createView

--- books/views.py
+++ books/views.py
@@ -13,3 +13,9 @@ class BookListView(generic.ListView):
 class BookDetailView(generic.DetailView):
     model = Book
     template_name = "books/book_detail.html"
+
+class BookCreateView(generic.CreateView):
+    model = Book
+    fields = ['title', 'author', 'content', 'price',]
+    template_name = "books/book_create.html"
+
