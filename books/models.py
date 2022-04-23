c3b1e52ff2717053327eea56c4bc4eb698d8fef1
Shahrokh Kiyani <shahrokhkiyani@protonmail.com>
2022-04-23 20:47:56 +0430

    added upload to site

--- books/models.py
+++ books/models.py
@@ -15,6 +15,7 @@ class Book(models.Model):
 
     # ? For money we use DeciamlFields
     price = models.DecimalField(max_digits=5, decimal_places=2)
+    cover = models.ImageField(upload_to="covers/", blank=True)
 
     def __str__(self):
         return f"{self.title}, {self.author}, {self.price}"
