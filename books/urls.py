e0fd4d134fcf3fae640b99bd527403051a39a924
Shahrokh Kiyani <shahrokhkiyani@protonmail.com>
2022-04-29 10:38:37 +0430

    added comments to detail page

--- books/urls.py
+++ books/urls.py
@@ -3,14 +3,14 @@ from django.urls import path
 from .views import (
     BookCreateView,
     BookListView,
-    BookDetailView,
+    book_detail_view,
     BookUpdateView,
     BookDeleteView,
 )
 
 urlpatterns = [
     path("", BookListView.as_view(), name="book_list"),
-    path("<int:pk>/", BookDetailView.as_view(), name="book_detail"),
+    path("<int:pk>/", book_detail_view, name="book_detail"),
     path("new/", BookCreateView.as_view(), name="book_create"),
     path("<int:pk>/edit/", BookUpdateView.as_view(), name="book_update"),
     path("<int:pk>/delete/", BookDeleteView.as_view(), name="book_delete"),
--- books/views.py
+++ books/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404
 
 from .models import Book
 
@@ -12,9 +13,14 @@ class BookListView(generic.ListView):
     context_object_name = "books"
 
 
-class BookDetailView(generic.DetailView):
-    model = Book
-    template_name = "books/book_detail.html"
+
+def book_detail_view(request, pk):
+    # get book comments
+
+    book = get_object_or_404(Book, pk=pk)
+    book_comment = book.comments.all()
+    
+    return render(request, "books/book_detail.html", {"book": book, "comments": book_comment})
 
 
 class BookCreateView(generic.CreateView):
